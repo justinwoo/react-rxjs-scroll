0a825f0ec0c25953345ec300a6166c7ac6de4d41
Justin Woo <moomoowoo@gmail.com>
2015-05-03 17:12:26 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-rxjs-scroll
+
+rough translation of http://www.simple.gy/blog/infinite-bacon/
---
+++ src/index.js
@@ -0,0 +1,105 @@
+var Rx = require('rx');
+var React = require('react');
+
+var getScrollY = function () {
+  return window.scrollY;
+};
+
+var initialScrollSubject = (new Rx.ReplaySubject(1));
+initialScrollSubject.onNext(getScrollY());
+
+var scrollYStream = initialScrollSubject.merge(
+  Rx.Observable.fromEvent(window, 'scroll').map(getScrollY)
+);
+
+var getWindowHeight = function () {
+  return window.innerHeight;
+};
+
+var initialHeightSubject = (new Rx.ReplaySubject(1));
+initialHeightSubject.onNext(getWindowHeight());
+
+var windowHeightStream = initialHeightSubject.merge(
+  Rx.Observable.fromEvent(window, 'resize').map(getWindowHeight)
+);
+
+var Phonebook = React.createClass({
+  getInitialState: function () {
+    return {
+      visibleIndices: []
+    };
+  },
+
+  render: function () {
+    var {totalResults, rowHeight} = this.props;
+    var elements = this.state.visibleIndices.map(function (index, i) {
+      return (
+        <li
+          key={i}
+          style={{
+            top: index * rowHeight
+          }}>
+          {index}
+        </li>
+      );
+    });
+
+    return (
+      <div>
+        <ul
+          className="phonebook"
+          style={{
+            height: totalResults * rowHeight
+          }}>
+          {elements}
+        </ul>
+      </div>
+    );
+  },
+
+  componentDidMount: function () {
+    var {rowHeight, totalResults} = this.props;
+
+    var firstVisibleRowStream = scrollYStream.map(function (y) {
+      return Math.floor(y / rowHeight);
+    }).distinctUntilChanged();
+
+    var windowHeightStream = this.props.windowHeightStream.distinctUntilChanged();
+    var rowCountStream = windowHeightStream.map(function (screenHeight) {
+      return Math.ceil(screenHeight / rowHeight);
+    }).distinctUntilChanged();
+
+    var visibleIndicesStream = Rx.Observable.combineLatest(
+      firstVisibleRowStream,
+      rowCountStream,
+      function (firstRow, rowCount) {
+        var visibleIndices = [];
+
+        // Limit the number of visible rows
+        var lastRow = firstRow + rowCount + 1;
+        if (lastRow > totalResults) {
+          firstRow -= lastRow - totalResults;
+        }
+
+        for (var i = 0; i <= rowCount; i++) { visibleIndices.push(i + firstRow) }
+        return visibleIndices;
+      }
+    );
+
+    this.visibleIndicesSubscription = visibleIndicesStream.subscribe((indices) => {
+      this.setState({
+        visibleIndices: indices
+      });
+    });
+  }
+});
+
+React.render(
+  <Phonebook
+    totalResults={10000}
+    rowHeight={30}
+    scrollYStream={scrollYStream}
+    windowHeightStream={windowHeightStream}
+  />,
+  document.getElementById('app')
+);
